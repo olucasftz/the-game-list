60e75a904cb726043260a21f8764a1440622842e
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-05-16 17:55:00 -0300

    project created

---
+++ src/main/java/com/devsuperior/thegamelist/ThegamelistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.thegamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThegamelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/thegamelist/ThegamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.thegamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
