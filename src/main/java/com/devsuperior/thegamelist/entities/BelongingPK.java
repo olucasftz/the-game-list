5e06c5957fa19e024a9a6f0f1d6f38ff420dbd02
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-05-17 19:16:39 -0300

    feat: pt2 domain model, Belonging and BelongingPK classes, Merging two associated ids using EmbeddedId and Embeddable

---
+++ src/main/java/com/devsuperior/thegamelist/entities/BelongingPK.java
@@ -0,0 +1,48 @@
+package com.devsuperior.thegamelist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.ManyToMany;
+
+public class BelongingPK {
+
+	@ManyToMany
+	private Game game;
+	
+	@ManyToMany
+	private GameList list;
+	
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public GameList getList() {
+		return list;
+	}
+
+	public void setList(GameList list) {
+		this.list = list;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+}
