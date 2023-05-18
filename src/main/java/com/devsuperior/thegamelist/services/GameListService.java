99bef1f32a158cc92e243bb37e337ce60738f9ea
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-05-18 20:29:23 -0300

    feat: moving games in the list with order updating in the database

--- src/main/java/com/devsuperior/thegamelist/services/GameListService.java
+++ src/main/java/com/devsuperior/thegamelist/services/GameListService.java
@@ -8,17 +8,38 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.thegamelist.dto.GameListDTO;
 import com.devsuperior.thegamelist.entities.GameList;
+import com.devsuperior.thegamelist.projections.GameMinProjection;
 import com.devsuperior.thegamelist.repositories.GameListRepository;
+import com.devsuperior.thegamelist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> gameList = gameListRepository.findAll();
 		return gameList.stream().map(GameListDTO::new).toList(); // passing GameList to DTO 
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
